492cb8fe770fef12ac8ca8f1c291413f3f0c82df
Gregory Daynes <gregdaynes@gmail.com>
2021-07-15 21:14:14 -0700

    feature: MVP implementation
    Web boundary with 3 endpoints:
    
    GET /
    returns a simple form that takes a url and sends it to the POST
    endpoint. Form request payload is encoded as text/plain to avoid having
    to handle multipart on the server.
    
    POST /
    creates a hash of a received url in the body payload. Then looks up the
    hash and url submitted against a list of existing urls in memory. If the
    hash and url entry exists in the memory, it is returned immediately in
    the format of a href string. Otherwise, the entry is appended to the
    file `urls` in the app root, as well as appended to the urls in memory.
    
    GET /:hash
    Looks up the hash value against the urls in memory, if found it returns
    html content that includes a meta refresh tag which will redirect the
    browser to the stored url.

--- index.mjs
+++ index.mjs
@@ -1,3 +1,69 @@
 #!/usr/bin/env nodejs
 
-console.log("ready");
+import fs from "node:fs/promises"
+import { createHmac } from "node:crypto"
+import Fastify from "fastify"
+
+let urls = await (async function () {
+  try {
+    const fileBuffer = await fs.readFile("urls")
+
+    return new Set([
+      ...fileBuffer
+        .toString()
+        .split(/\r?\n/)
+        .filter((x) => x !== ""),
+    ])
+  } catch {
+    return new Set([])
+  }
+})()
+
+const fastify = Fastify({ logger: true })
+
+fastify.get("/", {}, (request, reply) => {
+  return reply.code(200).header("Content-Type", "text/html; charset=utf-8")
+    .send(`
+    <!doctype html>
+    <body>
+    <form method="post" action="/" enctype="text/plain">
+    <input name="url">
+    <input type="submit">
+  `)
+})
+
+fastify.post("/", {}, async (request, reply) => {
+  const url = request.body.split("=")[1]
+
+  const hash = createHmac("sha256", "secret")
+    .update(url)
+    .digest("hex")
+    .slice(0, 8)
+
+  const entry = `${hash}:${url}`
+  const link = `http://localhost:3000/${hash}`
+
+  try {
+    await fs.appendFile("urls", entry)
+    urls.add(entry)
+
+    return link
+  } catch (err) {
+    fastify.log.error(err)
+  }
+})
+
+fastify.get("/:hash", {}, (request, reply) => {
+  const matcher = new RegExp(`^${request.params.hash}`)
+  const url = Array.from(urls)
+    .filter((x) => matcher.test(x))[0]
+    .substring(9)
+
+  return reply.code(200).header("Content-Type", "text/html; charset=utf-8")
+    .send(`
+    <!doctype html>
+    <meta http-equiv="refresh" content="0; URL='${url}'" />
+  `)
+})
+
+await fastify.listen("3000")
